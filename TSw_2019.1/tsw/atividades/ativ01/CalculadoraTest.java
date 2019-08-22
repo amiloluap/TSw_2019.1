db594e8fd5aa906d6d9fb759ebbb60c378d6c3ed
Paulo <Paulo@MVBRRECNTB.mvrec.local>
2019-08-21 21:56:59 -0300

    Esqueleto da classe de Teste criado

---
+++ TSw_2019.1/tsw/atividades/ativ01/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package atividades.ativ01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@Test
+	void testDividir() {
+		fail("Not yet implemented");
+	}
+
+}
